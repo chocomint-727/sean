d40c5d0e108f41ac41971a3bb358940143902637
3toadbanana <aditgarg1508@gmail.com>
2025-12-10 20:25:22 +0100

    home screen sorry uncle

--- app/src/main/java/hu/bme/ait/sean/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/hu/bme/ait/sean/ui/screen/home/HomeScreen.kt
@@ -1,13 +1,10 @@
 package hu.bme.ait.sean.ui.screen.home
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.auth.User
 
 @Composable
 fun HomeScreen(
